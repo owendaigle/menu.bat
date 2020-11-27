ce9eaf015bde69cc863cae433060425af9fa176a
Elgiad Newo <52384666+TheRuntingMuumuu@users.noreply.github.com>
2020-11-27 13:57:13 -0500

    Add files via upload

---
+++ Menu_Installer.bat
@@ -0,0 +1,222 @@
+ECHO OFF
+CLS
+Title MENU Download
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+ECHO This will make a folder called Menu inside the folder of this file, and then it will install the program in that folder.
+timeout 8
+
+mkdir Menu
+cd Menu
+
+ECHO Downloading apps
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1Xxl2_KRsfC7piAAB8-nKQZ28ur0oE0MG', 'Apps.bat')"
+sleep 4
+
+
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+ECHO Downloading Extras
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1tytotWK_aInu5zj8sNN6ShuPAnP_DfZr', 'Extras.bat')"
+sleep 3
+
+
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+
+ECHO Downloading functions
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1FgRtmGMHBlQH6iBjjmRHxacS8h6BNd5r', 'Functions.bat')"
+sleep 2
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+ECHO Downloading number genarator
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=17yUR4nMjMuEG2zndBpcfhT5ZMV6rIRKI','Genarator.bat')"
+sleep 3
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+
+ECHO Downloading computer locker
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1oPV9Jvh6KamCKrLTHUfqqtPro6FRt0Fg', 'Lock.bat')"
+sleep 1
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+
+ECHO Downloading main menu
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=17Bjrnl2GqaYHT92NQ-92-fuO7xEW8lFy', 'menu.bat')"
+sleep 10
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+
+ECHO Downloading start
+ECHO DO NOT QUIT OR SHUTDOWN COMPUTER
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1d2pCUCpNaJjZlpgYk6nutHN7BbwPnXd9', 'STARTMENU.bat')"
+sleep 2
+
+cls
+
+
+:Start
+ECHO  .     .   
+ECHO  ..   ..   ....  
+ECHO  . . . .   .      .    .   
+ECHO  .  .  .   .      ..   .   .    .
+ECHO  .     .   ....   . .  .   .    .
+ECHO  .     .   .      .  . .   .    .
+ECHO            ....   .   ..   .    .
+ECHO                   .    .   .    .
+ECHO                             ....
+color E
+ECHO         Made by Elgiad Newo
+ECHO(
+ECHO(
+
+
+
+
+
+ECHO You have downloaded MENU. Find the STARTMENU file in the Menu folder and run it. You may delete this file if you want.
+pause
+
+exit 0
